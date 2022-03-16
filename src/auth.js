a2ce29e422623d19532b7514c5e13e56ce0889c1
@mobolade <@mobolade.akinlua@outlook.com>
2022-03-16 15:26:33 +0100

    fix: auth

--- src/auth.js
+++ src/auth.js
@@ -16,27 +16,26 @@ async function UrlClient(endpoint, data, method) {
   }
 }
 
+const localStorageKey = "access-token";
 
-const localStorageKey = 'access-token'
-
-async function getToken () {
-    return localStorage.getItem(localStorageKey)
+async function getToken() {
+  return localStorage.getItem(localStorageKey);
 }
 
-function handleResponse (user) {
-    localStorage.setItem(localStorageKey, user.token)
-    return user
+function handleResponse(user) {
+  localStorage.setItem(localStorageKey, user.token);
+  return user;
 }
 
-async function signIn (body,method) {
- const data = await UrlClient ('signin', body, method)
- return handleResponse(data)
+async function signIn(body, method) {
+  const data = await UrlClient("signin", body, method);
+  return handleResponse(data);
 }
 
-async function signUp (body,method) {
-    const data = await UrlClient ('signup', body, method)
-   }
-   
-   async function logOut (){
-       return localStorage.removeItem(localStorageKey)
-   }
+async function signUp(body, method) {
+  const data = await UrlClient("signup", body, method);
+}
+
+async function logOut() {
+  return localStorage.removeItem(localStorageKey);
+}
