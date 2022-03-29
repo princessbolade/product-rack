3503d4488141f88c1f7ab34c324b398832f77f25
@mobolade <@mobolade.akinlua@outlook.com>
2022-03-29 10:45:23 +0100

    feat:display login user and logout implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AuthApp from "./authenticated-app";
 import Home from "./components/home";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/shelf" element={<AuthApp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -1,1 +1,47 @@
+import {
+  Box,
+  Button,
+  Flex,
+  ListItem,
+  UnorderedList,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
 
+function AuthApp() {
+  const authUser = JSON.parse(localStorage.getItem("user"));
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+  return (
+    <Box mx={"auto"} maxW={"1440px"} py={"50px"}>
+      <Flex alignItems={"center"} justifyContent={"flex-end"}>
+        <Text>Welcome, {authUser.username}!</Text>
+        <Button colorScheme={"green"} ml={"50px"} onClick={logout()}>
+          Logout
+        </Button>
+      </Flex>
+      <Box mx={"auto"} maxW={"800px"} py={"150px"}>
+        <Flex>
+          <Box>
+            <UnorderedList>
+              <ListItem>
+                <Text>HI</Text>
+              </ListItem>
+              <ListItem>
+                <Text>HI</Text>
+              </ListItem>
+              <ListItem>
+                <Text>HI</Text>
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
+
+export default AuthApp;
